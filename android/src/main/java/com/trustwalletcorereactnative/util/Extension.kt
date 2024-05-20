aa11412d0244dc05502d13d02a0313c4384fd293
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2024-05-20 09:29:04 +0700

    #added android util

---
+++ android/src/main/java/com/trustwalletcorereactnative/util/Extension.kt
@@ -0,0 +1,36 @@
+package com.trustwalletcorereactnative.util
+import android.util.Base64
+import com.google.protobuf.ByteString
+
+
+fun ByteArray.toHex(): String {
+    return Numeric.toHexString(this)
+}
+
+fun String.toHexBytes(): ByteArray {
+    return Numeric.hexStringToByteArray(this)
+}
+
+fun String.toHexByteArray(): ByteArray {
+    return Numeric.hexStringToByteArray(this)
+}
+
+fun String.toByteString(): ByteString {
+    return ByteString.copyFrom(this, Charsets.UTF_8)
+}
+
+fun String.toHexBytesInByteString(): ByteString {
+    return ByteString.copyFrom(this.toHexBytes())
+}
+
+fun ByteArray.base64String(): String {
+    return Base64.encodeToString(this, Base64.DEFAULT).toString()
+}
+
+fun Any.toLong(): Long {
+    return if(this is Int){
+        this.toLong()
+    }else{
+        this as Long
+    }
+}
