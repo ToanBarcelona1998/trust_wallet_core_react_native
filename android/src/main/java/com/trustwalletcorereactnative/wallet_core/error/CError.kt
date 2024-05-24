d981fc7664afb212f8e510a2978e98f7076539e6
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2024-05-24 10:22:24 +0700

    #define android exception

---
+++ android/src/main/java/com/trustwalletcorereactnative/wallet_core/error/CError.kt
@@ -0,0 +1,23 @@
+package com.trustwalletcorereactnative.wallet_core.error
+
+enum class CEError(val message : String,val code :String){
+    MissingArgumentError("Missing argument error", "MissingArgumentError"),
+    InvalidHexArgumentError("Invalid hex argument error","InvalidHexArgumentError"),
+    UnSupportedCoin("Unsupported coin","UnSupportedCoin"),
+    InvalidMnemonic("Invalid mnemonic","InvalidMnemonic"),
+    Unknown("Unknown error","Unknown"),
+    UnsupportedTransaction("UnSupported Transaction","UnsupportedTransaction")
+}
+
+
+final class CError : Exception{
+    private var code : String
+
+    constructor(message: String,code: String): super(message){
+        this.code = code
+    }
+
+    public fun getCode() : String{
+        return code
+    }
+}
