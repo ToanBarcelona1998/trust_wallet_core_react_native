7eb0f5670310259be6a1be0ff9c0ab1e3de78c86
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2024-06-25 11:43:17 +0700

    #update export parameter

--- src/parameter/index.tsx
+++ src/parameter/index.tsx
@@ -1,5 +1,8 @@
 import * as Ethereum from './ethereum';
 import * as SOL from './sol';
+import * as Bitcoin from './bitcoin';
+import * as Binance from './binance';
+import * as Cardano from './cardano';
 import { Parameter } from './parameter';
 
-export { Parameter, SOL, Ethereum };
+export { Parameter, SOL, Ethereum, Bitcoin, Binance, Cardano };
