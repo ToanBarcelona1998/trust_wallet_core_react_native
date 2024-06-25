ca781961e3805b146405b9c20e605d74c99112e4
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2024-06-25 14:13:15 +0700

    #named type

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import { createMnemonic, validateMnemonic } from './mnemonic';
-import * as Coin from './coin';
+import * as Mnemonic from './mnemonic';
+import * as TrustWalletCore from './coin';
 import { CoinType } from './coin_type';
 import * as CoinParameter from './parameter/index';
 
-export { validateMnemonic, createMnemonic, Coin, CoinType, CoinParameter };
+export { Mnemonic, TrustWalletCore, CoinType, CoinParameter };
